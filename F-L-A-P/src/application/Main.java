d047ca60600f951ea7e5f48c097e24639baf25d5
IvyBrandyn <IvyBrandyn@users.noreply.github.com>
2019-04-18 14:02:34 -0500

    Added background music and fixed a couple things.

--- F-L-A-P/src/application/Main.java
+++ F-L-A-P/src/application/Main.java
@@ -1,12 +1,15 @@
 package application;
 	
 import java.io.File;
+
 import javafx.application.Application;
+import javafx.concurrent.Task;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
+import javafx.scene.media.AudioClip;
 
 /**
  * Main.java
@@ -26,6 +29,21 @@ public class Main extends Application {
 	
 	@Override
 	public void start(Stage primaryStage) {
+		
+		// Play Music in background
+		final Task<Object> music = new Task<Object>() {
+
+	        @Override
+	        protected Object call() throws Exception {
+	            AudioClip audio = new AudioClip(getClass().getResource("data/music.mp3").toExternalForm());
+	            audio.setCycleCount(100);
+	            audio.play();
+	            return null;
+	        }
+	    };
+	    Thread thread = new Thread(music);
+	    thread.start();
+	    
 		//System.out.print(javafx.scene.text.Font.getFamilies());
 		//Add Application icon and convert from file to image
 		File iconFile = new File("src/application/data/images/icon.png");
@@ -51,4 +69,5 @@ public class Main extends Application {
 	public static void main(String[] args) {
 		launch(args);
 	}
+	
 }
